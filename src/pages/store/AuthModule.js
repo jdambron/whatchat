a11364795ba5568bf22abd4115839ab9c162b6ee
jdambron <jdambron@users.noreply.github.com>
2021-08-16 17:11:40 +0200

    feat: add email verification on sign up

--- src/pages/store/AuthModule.js
+++ src/pages/store/AuthModule.js
@@ -13,7 +13,7 @@ const AuthModule = {
         }
     },
     actions:{
-        signUp({commit},payload){
+        signUp({commit, dispatch},payload){
             firebase.auth().createUserWithEmailAndPassword(payload.email,payload.password)
                 .then(data=>{
                     firebase.database().ref('users').child(data.user.uid).set({
@@ -27,6 +27,7 @@ const AuthModule = {
                         displayName:payload.name
                     })
                         .then(()=>{
+                            dispatch('sendVerification')
                             commit('setSignedUp',true)
                             console.log('updated profile')
                         }).catch(err=>{
@@ -38,6 +39,14 @@ const AuthModule = {
                     console.log(err.message)
                     commit('setAlertMessage', err.message);
                 })
+        },
+        sendVerification({commit}){
+            var user = firebase.auth().currentUser;
+            user.sendEmailVerification()
+                .then(() => {
+                    // Email verification sent!
+                    commit('setAlertMessage', `A verification email has been been sent to ${user.email}`)
+                });
         }
     }
 }
