8ec43ebf771acff6d452a1a3e0fdf60f3066b45e
jdambron <jdambron@users.noreply.github.com>
2021-08-17 18:46:25 +0200

    feat: add resend verification email function

--- src/pages/auth/signin.vue
+++ src/pages/auth/signin.vue
@@ -1,6 +1,6 @@
 <template>
   <f7-page name="signin">
-    <f7-navbar title="Sign in" back-link="Back"></f7-navbar>
+    <f7-navbar title="Sign in"></f7-navbar>
     <f7-list inline-labels no-hairlines-md>
       <f7-list-input
         :value="email"
@@ -25,7 +25,7 @@
       <f7-button outline @click="signIn">Sign in</f7-button>
     </f7-block>
     <div style="text-align:center;">
-      <f7-link>Resend confirmation email</f7-link><br>
+      <f7-link @click="resendEmail">Resend confirmation email <span v-if="time_left>0">{{time_left}}</span></f7-link><br>
       <f7-link href="/signup/">Don't have an account? Sign up</f7-link><br>
       <f7-link>Forgot password</f7-link>
     </div>
@@ -36,10 +36,28 @@ export default {
   data(){
     return {
       email:null,
-      password:null
+      password:null,
+      time_left:-1
     }
   },
   methods:{
+    resendEmail(){
+      const self = this
+      if (self.time_left<=0) {
+        self.$store.dispatch('sendVerification')
+        self.countDown()
+      }
+    },
+    countDown(){
+      const self = this
+      self.time_left = 60
+      var timer = setInterval(function(){
+        self.time_left-=1
+        if (self.time_left <= 0){
+          clearInterval(timer)
+        }
+      },1000)
+    },
     signIn(){
       var payload = {}
       payload.email = this.email
